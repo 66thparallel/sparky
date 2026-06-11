8bdb3554d425cece6e8aa814bf848f75ae131e8d
66thparallel <38669412+66thparallel@users.noreply.github.com>
2026-06-10 21:13:06 -0700

    Added a startup.sh

---
+++ startup.sh
@@ -0,0 +1,6 @@
+# Startup bash script for the Sparky project.
+
+source /home/janeikeliu/projects/venv_sparky/bin/activate
+source /opt/ros/jazzy/setup.bash
+cd ~/projects/sparky
+source install/setup.bash
