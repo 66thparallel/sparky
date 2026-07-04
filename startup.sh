47efcdc3658e8e7b25dd572eb9ad3571a4e6225f
66thparallel <38669412+66thparallel@users.noreply.github.com>
2026-07-03 17:08:41 -0700

    Cleaned up files no longer used.

--- startup.sh
+++
@@ -1,6 +0,0 @@
-# Startup bash script for the Sparky project.
-
-cd ~/projects/sparky
-source /home/janeikeliu/projects/sparky/venv/bin/activate
-source /opt/ros/jazzy/setup.bash
-source install/setup.bash
